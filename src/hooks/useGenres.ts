b60920236313aaf577a77c0e87ccc84e7b61b8e5
ashutosh-gole <ashutoshsgole@gmail.com>
2024-08-17 17:46:54 +0530

    load genres using react query

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genres";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "./useData";
 // import useData from "./useData";
 
 export interface Genre {
@@ -11,6 +14,14 @@ export interface Genre {
 // const useGenres = () => useData<Genre>("/genres");
 // or
 // load genres using static data
-const useGenres = () => ({ data: genres, error: null, isLoading: false });
+// const useGenres = () => ({ data: genres, error: null, isLoading: false });
+// or
+// load genres using react query
+const useGenres = () => useQuery({
+    queryKey: ["genres"],
+    queryFn: () => apiClient.get<FetchResponse<Genre>>("/genres").then(res => res.data),
+    staleTime: 24 * 60 * 60 * 1000, //24h
+    initialData: { count: genres.length, results: genres }
+});
 
 export default useGenres;
