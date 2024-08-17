046b67a4c6b50957711edcccd18a2287ea8c4f94
ashutosh-gole <ashutoshsgole@gmail.com>
2024-08-17 17:58:41 +0530

    fetch platforms using react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { Box, Flex, Grid, GridItem, Show } from "@chakra-ui/react";
 import { useState } from "react";
 import GameGrid from "./components/GameGrid";
+import GameHeading from "./components/GameHeading";
 import GenreList from "./components/GenreList";
 import Navbar from "./components/Navbar";
 import PlatformSelector from "./components/PlatformSelector";
 import SortSelector from "./components/SortSelector";
 import { Genre } from "./hooks/useGenres";
 import { Platform } from "./hooks/usePlatforms";
-import GameHeading from "./components/GameHeading";
 
 export interface GameQuery {
   genre: Genre | null;
--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -18,7 +18,7 @@ const PlatformSelector = ({ onSelectedPLatform, selectedPlatform }: Props) => {
         {selectedPlatform?.name || "Platforms"}
       </MenuButton>
       <MenuList>
-        {data.map((platform) => (
+        {data?.results.map((platform) => (
           <MenuItem
             key={platform.id}
             onClick={() => onSelectedPLatform(platform)}
