244ff9d812e5f0c1d330afd5e8360a90c297479f
ashutosh-gole <ashutoshsgole@gmail.com>
2024-08-22 00:08:15 +0530

    fecth game details

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosRequestConfig } from "axios"
+import axios, { AxiosRequestConfig } from "axios";
 
 export interface FetchResponse<T> {
     count: number;
@@ -22,6 +22,10 @@ class APIClient<T> {
 
     getAll = (config: AxiosRequestConfig) => {
         return axiosInstance.get<FetchResponse<T>>(this.endpoint, config).then(res => res.data)
+    }
+
+    get = (id: string | number) => {
+        return axiosInstance.get<T>(`${this.endpoint}/${id}`).then(res => res.data);
     }
 
 }
